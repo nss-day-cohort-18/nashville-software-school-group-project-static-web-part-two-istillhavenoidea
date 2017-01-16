d2eccacbae0d14bacad4bed459e6113395e0b438
William A Sims III <willsims14@gmail.com>
2017-01-16 15:04:15 -0600

    Styles and created structure for products.html. Also populated each card from javascript/main.js

---
+++ javascript/main.js
@@ -0,0 +1,205 @@
+var earPlugs = [];
+
+
+// Plug #1
+var plug1 = {
+	name: "Plug #1",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+// Plug #2
+var plug2 = {
+	name: "Plug #2",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+
+// Plug #3
+var plug3 = {
+	name: "Plug #3",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+
+// Plug #4
+var plug4 = {
+	name: "Plug #4",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+// Plug #5
+var plug5 = {
+	name: "Plug #5",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+// Plug #6
+var plug6 = {
+	name: "Plug #6",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+// Plug #7
+var plug7 = {
+	name: "Plug #7",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+// Plug #8
+var plug8 = {
+	name: "Plug #8",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+
+// Plug #9
+var plug9 = {
+	name: "Plug #9",
+	description: "blah blah blah blah blah blah",
+	price: 5,
+	imgUrl: ""
+};
+
+
+// <name>				"plugName#"
+// <description>		"plugDesc#"
+// <price>				"plugPrice#"
+// <img>				"plugImg#"
+
+
+earPlugs.push(plug1);
+earPlugs.push(plug2);
+earPlugs.push(plug3);
+earPlugs.push(plug4);
+earPlugs.push(plug5);
+earPlugs.push(plug6);
+earPlugs.push(plug7);
+earPlugs.push(plug8);
+earPlugs.push(plug9);
+
+console.log("EarPlugs[i]: ", earPlugs);
+
+
+// Plug # 1
+var nameDOM1 = document.getElementById("plugName1");
+var descDOM1 = document.getElementById("plugDesc1");
+var priceDOM1 = document.getElementById("plugPrice1");
+var imageDOM1 = document.getElementById("plugImg1");
+
+nameDOM1.innerHTML = earPlugs[0].name;
+descDOM1.innerHTML = earPlugs[0].description;
+priceDOM1.innerHTML = earPlugs[0].price;
+imageDOM1.src = earPlugs[0].imgUrl;
+
+
+
+// Plug # 2
+var nameDOM2 = document.getElementById("plugName2");
+var descDOM2 = document.getElementById("plugDesc2");
+var priceDOM2 = document.getElementById("plugPrice2");
+var imageDOM2 = document.getElementById("plugImg2");
+
+nameDOM2.innerHTML = earPlugs[1].name;
+descDOM2.innerHTML = earPlugs[1].description;
+priceDOM2.innerHTML = earPlugs[1].price;
+imageDOM2.src = earPlugs[1].imgUrl;
+
+
+// Plug # 3
+var nameDOM3 = document.getElementById("plugName3");
+var descDOM3 = document.getElementById("plugDesc3");
+var priceDOM3 = document.getElementById("plugPrice3");
+var imageDOM3 = document.getElementById("plugImg3");
+
+nameDOM3.innerHTML = earPlugs[2].name;
+descDOM3.innerHTML = earPlugs[2].description;
+priceDOM3.innerHTML = earPlugs[2].price;
+imageDOM3.src = earPlugs[2].imgUrl;
+
+
+// Plug # 4
+var nameDOM4 = document.getElementById("plugName4");
+var descDOM4 = document.getElementById("plugDesc4");
+var priceDOM4 = document.getElementById("plugPrice4");
+var imageDOM4 = document.getElementById("plugImg4");
+
+nameDOM4.innerHTML = earPlugs[3].name;
+descDOM4.innerHTML = earPlugs[3].desc;
+priceDOM4.innerHTML = earPlugs[3].price;
+imageDOM4.src = earPlugs[3].imgUrl;
+
+
+// Plug # 5
+var nameDOM5 = document.getElementById("plugName5");
+var descDOM5 = document.getElementById("plugDesc5");
+var priceDOM5 = document.getElementById("plugPrice5");
+var imageDOM5 = document.getElementById("plugImg5");
+
+nameDOM5.innerHTML = earPlugs[4].name;
+descDOM5.innerHTML = earPlugs[4].description;
+priceDOM5.innerHTML = earPlugs[4].price;
+imageDOM5.src = earPlugs[4].imgUrl;
+
+
+
+// Plug # 6
+var nameDOM6 = document.getElementById("plugName6");
+var descDOM6 = document.getElementById("plugDesc6");
+var priceDOM6 = document.getElementById("plugPrice6");
+var imageDOM6 = document.getElementById("plugImg6");
+
+nameDOM6.innerHTML = earPlugs[5].name;
+descDOM6.innerHTML = earPlugs[5].description;
+priceDOM6.innerHTML = earPlugs[5].price;
+imageDOM6.src = earPlugs[5].imgUrl;
+
+// Plug # 7
+var nameDOM7 = document.getElementById("plugName7");
+var descDOM7 = document.getElementById("plugDesc7");
+var priceDOM7 = document.getElementById("plugPrice7");
+var imageDOM7 = document.getElementById("plugImg7");
+
+nameDOM7.innerHTML = earPlugs[6].name;
+descDOM7.innerHTML = earPlugs[6].description;
+priceDOM7.innerHTML = earPlugs[6].price;
+imageDOM7.src = earPlugs[6].imgUrl;
+
+// Plug # 8
+var nameDOM8 = document.getElementById("plugName8");
+var descDOM8 = document.getElementById("plugDesc8");
+var priceDOM8 = document.getElementById("plugPrice8");
+var imageDOM8 = document.getElementById("plugImg8");
+
+nameDOM8.innerHTML = earPlugs[7].name;
+descDOM8.innerHTML = earPlugs[7].description;
+priceDOM8.innerHTML = earPlugs[7].price;
+imageDOM8.src = earPlugs[7].imgUrl;
+
+
+// Plug # 9
+var nameDOM9 = document.getElementById("plugName9");
+var descDOM9 = document.getElementById("plugDesc9");
+var priceDOM9 = document.getElementById("plugPrice9");
+var imageDOM9 = document.getElementById("plugImg9");
+
+nameDOM9.innerHTML = earPlugs[8].name;
+descDOM9.innerHTML = earPlugs[8].description;
+priceDOM9.innerHTML = earPlugs[8].price;
+imageDOM9.src = earPlugs[8].imgUrl;
+
